004169b361a75de83ee0ab583823fd3a4aa19032
James Devlin <devlin.ja@husky.neu.edu>
2020-08-23 02:47:48 -0400

    Working Prototype
    Very rough prototype, but it works as a start.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use termion::{color, style};
 fn main() {
-    println!("Hello, world!");
+    print!("{}   ", color::Bg(color::Black));
+    print!("{}   ", color::Bg(color::Red));
+    print!("{}   ", color::Bg(color::Green));
+    print!("{}   ", color::Bg(color::Yellow));
+    print!("{}   ", color::Bg(color::Blue));
+    print!("{}   ", color::Bg(color::Magenta));
+    print!("{}   ", color::Bg(color::Cyan));
+    print!("{}   ", color::Bg(color::White));
+    println!();
+    print!("{}   ", color::Bg(color::LightBlack));
+    print!("{}   ", color::Bg(color::LightRed));
+    print!("{}   ", color::Bg(color::LightGreen));
+    print!("{}   ", color::Bg(color::LightYellow));
+    print!("{}   ", color::Bg(color::LightBlue));
+    print!("{}   ", color::Bg(color::LightMagenta));
+    print!("{}   ", color::Bg(color::LightCyan));
+    print!("{}   ", color::Bg(color::LightWhite));
+    println!("{}", style::Reset);
 }
