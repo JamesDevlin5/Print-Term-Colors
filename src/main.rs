8dbd3194f56d96186143540712cfae8bad4ff022
James Devlin <devlin.ja@husky.neu.edu>
2020-08-23 04:05:18 -0400

    Created Functions
    Divided the main function up a bit. Created two helper functions, one
    for printing a line of colors from a collection, and one for displaying
        a single color properly. The colors are then defined in a
        collection, and passed to these helpers.

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,46 @@
 use termion::{color, style};
-fn main() {
-    print!("{}   ", color::Bg(color::Black));
-    print!("{}   ", color::Bg(color::Red));
-    print!("{}   ", color::Bg(color::Green));
-    print!("{}   ", color::Bg(color::Yellow));
-    print!("{}   ", color::Bg(color::Blue));
-    print!("{}   ", color::Bg(color::Magenta));
-    print!("{}   ", color::Bg(color::Cyan));
-    print!("{}   ", color::Bg(color::White));
-    println!();
-    print!("{}   ", color::Bg(color::LightBlack));
-    print!("{}   ", color::Bg(color::LightRed));
-    print!("{}   ", color::Bg(color::LightGreen));
-    print!("{}   ", color::Bg(color::LightYellow));
-    print!("{}   ", color::Bg(color::LightBlue));
-    print!("{}   ", color::Bg(color::LightMagenta));
-    print!("{}   ", color::Bg(color::LightCyan));
-    print!("{}   ", color::Bg(color::LightWhite));
+
+/// Prints a single background color along a group of three space characters.
+/// This will effectively display a block of empty space that is colored by the supplied color.
+fn pr_color(c: &dyn color::Color) {
+    print!("{}   ", color::Bg(c));
+}
+
+/// Prints a single line in accordance with the colors provided.
+/// This function will ensure proper formatting of the line printed, included prefixed padding, printing of each color in the collection properly, and cleaning up after the line if finished.
+fn pr_line(cvec: &Vec<&dyn color::Color>) {
+    print!("   ");
+    for c in cvec {
+        pr_color(c);
+    }
     println!("{}", style::Reset);
+}
+
+fn main() {
+    // A collection of all the regular colors
+    let colors: Vec<&dyn color::Color> = vec![
+        &color::Black,
+        &color::Red,
+        &color::Green,
+        &color::Yellow,
+        &color::Blue,
+        &color::Magenta,
+        &color::Cyan,
+        &color::White,
+    ];
+
+    // A collection of all the light colors
+    let lcolors: Vec<&dyn color::Color> = vec![
+        &color::LightBlack,
+        &color::LightRed,
+        &color::LightGreen,
+        &color::LightYellow,
+        &color::LightBlue,
+        &color::LightMagenta,
+        &color::LightCyan,
+        &color::LightWhite,
+    ];
+
+    pr_line(&colors);
+    pr_line(&lcolors);
 }
